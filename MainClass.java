9f00148d43492cd3f6deb5648381934bba17593e
vickeee15 <89180138+vickeee15@users.noreply.github.com>
2021-10-08 09:12:14 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -4,10 +4,13 @@ public class MainClass {
     public static void main(String[] args) {
     System.out.println("REGEX USER VALIDATION");
     Scanner sc= new Scanner(System.in);
-    RegexFname validate = new RegexFname();
+    Regex validate = new Regex();
     System.out.println("Enter first name:");
     String fname = sc.nextLine();
     validate.validateName(fname);
+    System.out.println("enter last name");
+    String lname = sc.nextLine();
+    validate.validateName(lname);
 
 }
 }
